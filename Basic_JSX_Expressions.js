b9dfeb0d9c5dcda969d1762e5950d1b67b2f1264
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2021-06-03 13:14:30 -0500

    Update Basic_JSX_Expressions.js

--- Basic_JSX_Expressions.js
+++ Basic_JSX_Expressions.js
@@ -1,6 +1,8 @@
+// we first import our libraries
+// ReactDOM.render() is the main function to output text to the screen
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Write code here:
+// here we send 2 arguments to the ReactDOM.render functions
 ReactDOM.render(<h1>Render me!</h1>,
 document.getElementById('app'))
